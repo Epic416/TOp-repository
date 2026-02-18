dbf9abc7c228bbc4a3625743672126b6bacdfbff
Epic416 <Epic416>
2026-02-18 22:35:09 +0800

    mm

---
+++ МДК.01.05 Основы программирования на языке С++/codes/DZ_27/aaaa.h
@@ -0,0 +1,7 @@
+#ifndef AAAA_H 
+#define AAAA_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+#endif
---
+++ МДК.01.05 Основы программирования на языке С++/codes/DZ_27/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "aaaa.h" 
+
+int main() {
+    int x = 10, y = 5;
+
+    std::cout << add(x, y) << std::endl;
+    std::cout << subtract(x, y) << std::endl;
+}
